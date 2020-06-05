daacce9130b6d47bdb24dce240eceefee20f8b58
Amanda Mok <a.mok@berkeley.edu>
2020-06-05 02:24:34 -0700

    add plots and table to RShiny

--- visualize_data/app.R
+++ visualize_data/app.R
@@ -4,17 +4,12 @@
 library(shiny)
 library(ggplot2)
 library(here)
-
-source(file.path(here(), "scripts/helper.R"))
-
-coverage_id <- "PRJNA616446"
-abundance_binSize <- 300
-abundance <- load_coverage(coverage_id, abundance_binSize)
+library(patchwork)
 
 # import data -------------------------------------------------------------
 
-plusStrand <- read.table(file.path(here(), "outputs/cas13a_20nt/cas13a_results_summary.txt"), stringsAsFactors=F)
-minusStrand <- read.table(file.path(here(), "outputs/cas13a_minusStrand_20nt/cas13a_minusStrand_results_summary.txt"), stringsAsFactors=F)
+plusStrand <- read.table(file.path(here(), "outputs/cas13a_20nt/cas13a_results_summary.txt"))
+minusStrand <- read.table(file.path(here(), "outputs/cas13a_minusStrand_20nt/cas13a_minusStrand_results_summary.txt"))
 dat <- rbind(plusStrand, minusStrand)
 
 # filter candidate guides
@@ -25,11 +20,23 @@ filtered_dat <- subset(filtered_dat, match_against_hg38==0) # n = 7104 (+: 3729;
 filtered_dat <- subset(filtered_dat, antitag != "GUUU") # n = 7047 (+: 3702; -: 3345)
 filtered_dat <- subset(filtered_dat, sensitivity_01 >= 0.95) # n = 6969 (+: 3678; -: 3291)
 
-# define UI ---------------------------------------------------------------
+# load viral abundance coverage data
+coverage_id <- "PRJNA616446"
+abundance_binSize <- 300
+id_cov <- read.table(file.path(here(), "ref_data/RNA_expression", paste0(coverage_id, "_mapped.cov")),
+                     header=F, stringsAsFactors=F, col.names=c("genome", "pos", "coverage"))
+id_cov$bin <- cut(id_cov$pos, breaks=ceiling(max(id_cov$pos)/abundance_binSize), labels=F)
+id_cov <- aggregate(coverage~bin, data=id_cov, FUN=median)
+id_cov$bin <- (id_cov$bin-1)*abundance_binSize + abundance_binSize/2
 
+# filter 
 filters <- c("All guides" = "dat",
              "Filtered guides" = "filtered_dat",
              "Ordered guides" = "ordered_dat")
+filter_label <- names(filters)
+names(filter_label) <- filters
+
+# features
 features <- c("Sensitivity (no mismatch)" = "sensitivity_0",
               "Sensitivity (1 mismatch)" = "sensitivity_01",
               "Specificity" = "specificity",
@@ -37,6 +44,22 @@ features <- c("Sensitivity (no mismatch)" = "sensitivity_0",
               "Spacer base-pairing" = "crRNA_spacer_basepairs",
               "crRNA hairpin intact" = "has_crRNA_hairpin",
               "Alignments to human transcriptome" = "match_against_hg38")
+feature_label <- names(features)
+names(feature_label) <- features
+feature_label["match_against_hg38"] <- "# alignments to \nhuman transcriptome"
+feature_desc <- c("% SARS-CoV-2 samples detected (no mismatches)",
+                  "% SARS-CoV-2 samples detected (1 mismatch)",
+                  "% human coronaviruses missed (2 mismatches)",
+                  "Spacer GC content",
+                  "# basepairs in spacer",
+                  "crRNA hairpin intact",
+                  "# alignments to human transcriptome")
+names(feature_desc) <- features
+
+# columns
+display_columns <- c("start", "strand", "target", "spacer", "antitag", "sensitivity_01", "specificity")
+
+# define UI ---------------------------------------------------------------
 
 ui <- fluidPage(
   
@@ -60,38 +83,56 @@ ui <- fluidPage(
     ),
     
     mainPanel(
-      # img(src="rstudio.png", height=140, width=400),
-      # plotOutput("diagnostic_plot"),
-      textOutput("selected_var")
+      plotOutput("scatter_plot"),
+      p(class="text-center", downloadButton('x3', "Download Data"),
+      DT::dataTableOutput("print_table"))
     )
   )
 )
 
 # define server logic -----------------------------------------------------
 
-filter_label <- names(filters)
-names(filter_label) <- filters
-feature_label <- names(features)
-names(feature_label) <- features
-feature_desc <- c("% SARS-CoV-2 samples detected (no mismatches)",
-                  "% SARS-CoV-2 samples detected (1 mismatch)",
-                  "% human coronaviruses missed (2 mismatches)",
-                  "Spacer GC content",
-                  "# basepairs in spacer",
-                  "crRNA hairpin intact",
-                  "# alignments to human transcriptome")
-names(feature_desc) <- features
-
 server <- function(input, output) {
   
-  # output$diagnostic_plot <- renderPlot(
-  #   plot_diagnostic(get(input$data_filter), abundance, filter=filter_label[input$data_filter], 
-  #                   alpha=0.5, jitter_x=0.001, jitter_y=0.05, 
-  #                   var1=input$var1, var1_name=feature_label[input$var1], var1_desc=feature_desc[input$var1],
-  #                   var2=input$var2, var2_name=feature_label[input$var2], var2_desc=feature_desc[input$var2],
-  #                   var3=input$var3, var3_name=feature_label[input$var3], var3_desc=feature_desc[input$var3]))
-
-  output$selected_var <- renderText({"You have selected this"})
+  output$print_table <- DT::renderDataTable({
+    plot_data <- get(input$data_filter)
+    plot_data <- subset(plot_data, start >= input$genome_range[1])
+    plot_data <- subset(plot_data, start <= input$genome_range[2]-20)
+    plot_data <- switch(input$strand_filter,
+                        "both" = plot_data,
+                        "plus" = subset(plot_data, strand=="+"),
+                        "minus" = subset(plot_data), strand=="-")
+    return(datatable(plot_data[, display_columns], rownames=F, selection="multiple"))
+  })
+  
+  output$scatter_plot <- renderPlot({
+    # subset data
+    plot_data <- get(input$data_filter)
+    plot_data <- subset(plot_data, start >= input$genome_range[1])
+    plot_data <- subset(plot_data, start <= input$genome_range[2]-20)
+    plot_data <- switch(input$strand_filter,
+                        "both" = plot_data,
+                        "plus" = subset(plot_data, strand=="+"),
+                        "minus" = subset(plot_data), strand=="-")
+    # scatter plot
+    scatter_plot <- ggplot(plot_data, aes_string(input$var1, input$var2, col=input$var3)) +
+      geom_jitter(width=(max(plot_data[input$var1])-min(plot_data[input$var1]))/20,
+                  height=(max(plot_data[input$var1])-min(plot_data[input$var1]))/20,
+                  alpha=0.5) +
+      theme_bw() + xlab(feature_label[input$var1]) + ylab(feature_label[input$var2]) +
+      scale_color_gradient2(high="red", low="blue", mid="grey", midpoint=mean(plot_data[,input$var3]),
+                            name=feature_label[input$var3]) +
+      ggtitle("Guide design scores", subtitle=paste("n =", nrow(plot_data)))
+    # univariate plots
+    var1_plot <- ggplot(plot_data, aes_string(input$var1)) + geom_density(kernel="gaussian", fill=2, col=2) +
+      theme_bw() + ggtitle(feature_label[input$var1]) + xlab(feature_desc[input$var1])
+    var2_plot <- ggplot(plot_data, aes_string(input$var2)) + geom_density(kernel="gaussian", fill=3, col=3) +
+      theme_bw() + ggtitle(feature_label[input$var2]) + xlab(feature_desc[input$var2])
+    var3_plot <- ggplot(plot_data, aes_string(input$var3)) + geom_density(kernel="gaussian", fill=4, col=4) +
+      theme_bw() + ggtitle(feature_label[input$var3]) + xlab(feature_desc[input$var3])
+    return(scatter_plot + (var1_plot / var2_plot / var3_plot) + plot_layout(widths=c(2,1)))
+  })
+  
 }
 
 
